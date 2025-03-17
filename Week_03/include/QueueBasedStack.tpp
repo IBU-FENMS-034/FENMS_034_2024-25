2aad1f3d9a41b23c16de21fca90314fd722e0148
lejla.brescic <lejla.brescic@stu.ibu.edu.ba>
2025-03-18 00:01:20 +0100

    lab task

---
+++ Week_03/include/QueueBasedStack.tpp
@@ -0,0 +1,36 @@
+#ifndef QUEUEBASEDSTACK_TPP
+#define QUEUEBASEDSTACK_TPP
+
+#include "QueueBasedStack.h"
+
+template<typename Data>
+QueueBasedStack<Data>::QueueBasedStack() {
+    // Constructor - Initialize any required data if needed
+}
+
+template<typename Data>
+void QueueBasedStack<Data>::push(const Data& data) {
+    // Implementation goes here
+}
+
+template<typename Data>
+Data QueueBasedStack<Data>::pop() {
+    // Implementation goes here
+}
+
+template<typename Data>
+Data QueueBasedStack<Data>::peek() const {
+    // Implementation goes here
+}
+
+template<typename Data>
+int QueueBasedStack<Data>::size() const {
+    // Implementation goes here
+}
+
+template<typename Data>
+bool QueueBasedStack<Data>::is_empty() const {
+    // Implementation goes here
+}
+
+#endif // QUEUEBASEDSTACK_TPP
--- Week_03/src/week_03.cpp
+++ Week_03/src/week_03.cpp
@@ -5,14 +5,18 @@
 #include <iostream>
 #include "../include/Stack.h"
 #include "../include/Queue.h"
-
+#include "../include/QueueBasedStack.h"
 void stack_demo();
 void queue_demo();
-
+void queue_based_stack_demo();
 int main() {
-    stack_demo();
-    std::cout << std::endl;
-    queue_demo();
+
+    queue_based_stack_demo();
+    //stack_demo();
+    //std::cout << std::endl;
+    //queue_demo();
+    return 0;
+
 }
 
 void stack_demo() {
@@ -71,4 +75,37 @@ void queue_demo() {
     std::cout << "- Removing: " << queue2.dequeue() << std::endl;
     std::cout << "- Removing: " << queue2.dequeue() << std::endl;
     std::cout << "- Removing: " << queue2.dequeue() << std::endl;
+}
+void queue_based_stack_demo() {
+    std::cout << "Queue-Based Stack Demo:" << std::endl;
+    QueueBasedStack<int> stack;
+
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    std::cout << "Top element: " << stack.peek() << std::endl;
+    std::cout << "Popping: " << stack.pop() << std::endl;
+    std::cout << "Popping: " << stack.pop() << std::endl;
+
+    stack.push(40);
+    std::cout << "Top element after pushing 40: " << stack.peek() << std::endl;
+
+    std::cout << "Stack size: " << stack.size() << std::endl;
+    std::cout << "Is stack empty? " << (stack.is_empty() ? "Yes" : "No") << std::endl;
+
+    std::cout << std::endl;
+
+    QueueBasedStack<char> stack2;
+    stack2.push('A');
+    stack2.push('B');
+    stack2.push('C');
+
+    std::cout << "Top of stack2: " << stack2.peek() << std::endl;
+    std::cout << "Removing: " << stack2.pop() << std::endl;
+    std::cout << "Removing: " << stack2.pop() << std::endl;
+    std::cout << "Removing: " << stack2.pop() << std::endl;
+
+    std::cout << "Is stack2 empty? " << (stack2.is_empty() ? "Yes" : "No") << std::endl;
+
 }
